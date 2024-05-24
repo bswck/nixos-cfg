5dbfa2b93fd35a56051e930605d9fbf20befd2d5
Bartosz Sławecki <bartoszpiotrslawecki@gmail.com>
2024-05-24 07:49:51 +0200

    Update wsl.nix

--- wsl.nix
+++ wsl.nix
@@ -1,6 +1,4 @@
 {
-  # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
-  # secrets,
   username,
   hostname,
   pkgs,
@@ -8,7 +6,7 @@
   ...
 }: {
   # FIXME: change to your tz! look it up with "timedatectl list-timezones"
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "Europe/Warsaw";
 
   networking.hostName = "${hostname}";
 
@@ -35,8 +33,7 @@
     shell = pkgs.zsh;
     extraGroups = [
       "wheel"
-      # FIXME: uncomment the next line if you want to run docker without sudo
-      # "docker"
+      "docker"
     ];
     # FIXME: add your own hashed password
     # hashedPassword = "";
@@ -97,12 +94,6 @@
   nix = {
     settings = {
       trusted-users = [username];
-      # FIXME: use your access tokens from secrets.json here to be able to clone private repos on GitHub and GitLab
-      # access-tokens = [
-      #   "github.com=${secrets.github_token}"
-      #   "gitlab.com=OAuth2:${secrets.gitlab_token}"
-      # ];
-
       accept-flake-config = true;
       auto-optimise-store = true;
     };
