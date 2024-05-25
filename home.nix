4787f74280479e54071d1575116ecc9c9bc26f50
Bartosz Sławecki <bartoszpiotrslawecki@gmail.com>
2024-05-25 12:44:57 +0200

    Change starship config

--- home.nix
+++ home.nix
@@ -119,16 +119,10 @@ in {
 
     starship.enable = true;
     starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
       git_branch.style = "242";
       directory.style = "blue";
       directory.truncate_to_repo = false;
       directory.truncation_length = 8;
-      python.disabled = true;
-      ruby.disabled = true;
-      hostname.ssh_only = false;
       hostname.style = "bold green";
     };
 
