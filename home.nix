29776b63e60898019de9973b89227c00bfc604a2
bswck <bartoszpiotrslawecki@gmail.com>
2024-05-28 19:06:05 +0200

    Add `notepad` alias

--- home.nix
+++ home.nix
@@ -214,6 +214,7 @@ in {
         pbcopy = "/mnt/c/Windows/System32/clip.exe";
         pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
         explorer = "/mnt/c/Windows/explorer.exe";
+        notepad = "/mnt/c/Windows/System32/notepad.exe";
       };
 
       envExtra = ''
